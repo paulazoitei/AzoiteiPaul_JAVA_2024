6fceaa0ab14678615f1caeac1b277d8eefac672c
paulazoitei <126176373+paulazoitei@users.noreply.github.com>
2024-05-28 08:25:54 +0300

    Add files via upload

---
+++ laborator12/Compulsory/src/main/java/org/example/Tests.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Tests {
+
+    @Test
+    public static void metodaDeTest1() {
+        System.out.println("metodaDeTest1 a fost executata");
+    }
+
+    @Test
+    public static void metodaDeTest2() {
+        System.out.println("metodaDeTest2 a fost executata");
+    }
+
+
+}
